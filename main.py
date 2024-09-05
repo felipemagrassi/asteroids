0fab43e9fe1b3617f7ae080cf4de0945e64bfd3e
Felipe Magrassi <felipe.1magrassi@gmail.com>
2024-09-04 22:26:20 -0300

    Created an empty black filled empty rectangle

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
